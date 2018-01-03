78133f76f5b3ad6e548582ca3f60673c73f62001
大黄 <chemwenxin@163.com>
2018-01-03 08:27:48 +0800

    添加一个主动刷新option的方法，界面不会闪动

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import { WebView, View } from 'react-native';
 import { Container, Echarts } from './components'
 
 export default class App extends Component {
+
+  setNewOption(option) {
+    this.chart.setNewOption(option);
+  }
+
   render() {
     return (
       <Container width={this.props.width}>
-        <Echarts {...this.props} />
+        <Echarts {...this.props} ref={e => this.chart = e}/>
       </Container>
     );
   }
